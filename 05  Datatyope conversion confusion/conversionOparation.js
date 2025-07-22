04bc01fe93f4277fb6c9c8607bd22cebecd7bce4
Abdur Rahman <abdurrahman262023.com>
2025-07-22 20:03:42 +0600

    conversion of datatypes

---
+++ 05  Datatyope conversion confusion/conversionOparation.js
@@ -0,0 +1,38 @@
+// let score = "33A";
+// let score = null;
+// let score = undefined;
+// let score = true;
+
+
+// * "33" -> 33
+// * "33A" -> NaN
+// * true -> 1; false -> 0 
+
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(typeof NaN);
+
+
+// let isloggedIn = "";
+// let booleanLoggedIn = Boolean(isloggedIn);
+
+// * 1 -> true; 0 -> false
+// * "" -> false
+// * "AR" -> true 
+
+// console.log(booleanLoggedIn);
+
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
